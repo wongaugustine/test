93168057ea214583f2e71ce260bf7ce43ddd7fd3
wongaugustine <72580849+wongaugustine@users.noreply.github.com>
2020-10-18 11:28:05 -0400

    Add files via upload

---
+++ scripts/notes.js
@@ -0,0 +1,27 @@
+let w1 = "W1 Course Intro and DCA Access - Welcome to CWMD1500 Web Technologies. This online course give you a high-level overview of the skills and abilities necessary to become a front-end web designer and developer. Focus is on providing you with a bigger picture of what is involved with creating web and interactive media projects. Some terminology and technology will be introduced followed by real-world processes covering – but not limited to – web design, web host management, project workflow and version control.";
+
+let w2="W2 This is John and here is my learning notes";
+
+let w3="W3 Developer Tools -notes to be completed by students";
+
+let w4="W4 Developer Repository -notes to be completed by students";
+
+let w5="W5 Service Provider over the Internet -notes to be completed by students";
+
+let w6="W6 Web Protocols -notes to be completed by students";
+
+let w7="W7 Site Publishing -notes to be completed by students";
+
+let w8="W8 Content Formats -notes to be completed by students";
+
+let w9="W9 Computer Languages -notes to be completed by students";
+
+let w10="W10 Language Framework and Library -notes to be completed by students";
+
+let w11="W11 Developer Collaborations -notes to be completed by students";
+
+let w12="W12 File Exchanges -notes to be completed by students";
+
+let w13="W13 Web Tech. Project -notes to be completed by students";
+
+let w14="W14 Final Exam";
